bead6ceff20f1950a968ff76e14c127e1387e34b
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-01-19 17:34:11 +0100

    added lazy instantation

--- socket/src/main/java/net/sf/lipermi/rmi/LocateRegistry.java
+++ socket/src/main/java/net/sf/lipermi/rmi/LocateRegistry.java
@@ -5,52 +5,101 @@ import net.sf.lipermi.Server;
 import net.sf.lipermi.exception.LipeRMIException;
 
 import java.io.IOException;
-import java.net.InetAddress;
+import java.util.Optional;
 
+import static java.lang.String.format;
 import static java.util.Optional.empty;
 
 /**
  *
  */
 public final class LocateRegistry {
+  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LocateRegistry.class);
 
-  static class ClientRegistry implements Registry{
-    final Client client;
+  static class RegistryImpl implements Registry {
+    private Optional<Client> lazyClient = empty();
+    private Optional<Server> lazyServer = empty();
 
-    public ClientRegistry(Client client) {
-      this.client = client;
+    private Optional<String> host = empty();
+    private final int port;
+
+    public RegistryImpl(int port) {
+      this.port = port;
+    }
+
+    public RegistryImpl(String host, int port) {
+      this.port = port;
+      this.host = Optional.of(host);
+    }
+
+    public void setHost( String value ) {
+      if( host.isPresent() ) {
+
+        if (!host.get().equalsIgnoreCase(value)) {
+          throw new IllegalStateException(format("host is already set %s with different given value %s!", host.get(), value));
+        }
+
+        return; // ignored
+      }
+
+      host = Optional.of(value);
     }
 
     @Override
     public void bind(Class<?> ifc, Object obj) throws LipeRMIException {
-      client.exportObject(ifc, obj);
-    }
+      log.trace( "bind {}", ifc.getName());
 
-    @Override
-    public <T> T lookup(Class<T> ifc) {
-      return client.getGlobal(ifc);
-    }
-  }
+      if(!lazyServer.isPresent()) {
 
+        try {
+          final Server server = new Server();
+          server.bind( port, UnicastRemoteObject.callHandler, empty() );
 
-  static class ServerRegistry implements Registry {
-    final Server server;
+          lazyServer = Optional.of(server);
 
-    public ServerRegistry(int port) throws IOException {
-      this.server = new Server();
-      server.bind(port, UnicastRemoteObject.callHandler, empty() );
-    }
+        } catch (IOException ex) {
+          log.error( "new Server error", ex );
+          throw new LipeRMIException(ex);
+        }
 
-    @Override
-    public void bind(Class<?> ifc, Object obj) throws LipeRMIException {
+      }
       UnicastRemoteObject.callHandler.registerGlobal( ifc, obj);
     }
 
     @Override
-    public <T> T lookup(Class<T> ifc) {
-      throw new UnsupportedOperationException("lookup is not supported yet!");
+    public <T> T lookup(Class<T> ifc) throws LipeRMIException  {
+      log.trace( "lookup {}", ifc.getName());
+
+      // WE ARE ON THE SERVER
+      if( lazyServer.isPresent() ) {
+
+        return UnicastRemoteObject.callHandler.getExportedObject(ifc)
+                .orElseThrow( () ->
+                        new LipeRMIException( format("exported object with interface %s not found!", ifc.getName())));
+      }
+
+      // WE ARE ON THE CLIENT
+      if( !host.isPresent() ) throw new IllegalStateException("host has not been set!");
+
+      if( !lazyClient.isPresent() ) {
+
+        try {
+
+          final Client client = new Client(host.get(), port, UnicastRemoteObject.callHandler, empty());
+          lazyClient = Optional.of(client);
+
+        } catch (IOException ex) {
+          log.error( "new Client error", ex );
+          throw new LipeRMIException(ex);
+        }
+      }
+      return lazyClient.get().getGlobal(ifc);
     }
+
   }
+
+  private static Optional<RegistryImpl> lazySingletonRegistry = empty();
+
 
   /**
    *
@@ -58,16 +107,13 @@ public final class LocateRegistry {
    * @return
    */
   public static Registry createRegistry(int port) throws LipeRMIException {
+    log.trace( "createRegistry( port:{} )", port);
 
-    try {
-
-      ServerRegistry server = new ServerRegistry(port);
-
-      return server;
-
-    } catch (IOException ex) {
-      throw new LipeRMIException(ex);
+    if( !lazySingletonRegistry.isPresent() ) {
+      final RegistryImpl reg = new RegistryImpl(port);
+      lazySingletonRegistry = Optional.of( reg );
     }
+    return lazySingletonRegistry.get();
   }
 
   /**
@@ -77,14 +123,21 @@ public final class LocateRegistry {
    */
   public static Registry getRegistry(String host, int port) throws LipeRMIException {
     if( host == null ) throw new IllegalArgumentException("host argument is null!");
+    log.trace( "getRegistry( host:{}, port:{} )", host, port);
 
     try {
 
-      final Client client = new Client(host, port, UnicastRemoteObject.callHandler, empty());
-      return new ClientRegistry(client);
+      if( !lazySingletonRegistry.isPresent() ) {
+        final RegistryImpl reg = new RegistryImpl(host, port);
+        lazySingletonRegistry = Optional.of( reg );
+      }
+      lazySingletonRegistry.get().setHost(host);
+      return lazySingletonRegistry.get();
     }
     catch( Exception ex ) {
-        throw new LipeRMIException(ex);
+      log.error( "getRegistry error", ex );
+
+      throw new LipeRMIException(ex);
     }
   }
 }
