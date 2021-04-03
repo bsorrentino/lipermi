7e894ff61be95716101b3781558172f6111e1c78
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-04-03 21:45:28 +0200

    add AbstractRegistry class in rmi emulation

--- socket/src/main/java/net/sf/lipermi/rmi/RMISocketRegistryImpl.java
+++ socket/src/main/java/net/sf/lipermi/rmi/RMISocketRegistryImpl.java
@@ -2,95 +2,26 @@ package net.sf.lipermi.rmi;
 
 import net.sf.lipermi.SocketClient;
 import net.sf.lipermi.SocketServer;
-import net.sf.lipermi.net.BaseClient;
-import net.sf.lipermi.net.IServer;
-
-import java.util.Optional;
-
-import static java.lang.String.format;
-import static java.util.Optional.ofNullable;
+import net.sf.lipermi.handler.CallHandler;
+import net.sf.lipermi.handler.filter.IProtocolFilter;
 
 /**
  *
  */
-public class RMISocketRegistryImpl implements Registry {
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(RMISocketRegistryImpl.class);
-
-    private BaseClient _client = null;
-    private IServer _server = null;
-
-    private String _host = null;
-    private final int port;
+public class RMISocketRegistryImpl extends AbstractRegistry {
 
     public RMISocketRegistryImpl(String host, int port ) {
-        this.port = port;
-        this._host = host;
-    }
-    public RMISocketRegistryImpl(int port ) {
-        this( null, port);
-    }
-
-    Optional<String> getHost() {
-        return ofNullable(_host);
-
+        super( host, port );
     }
 
     @Override
-    public <T, C extends T> void bind(Class<T> ifc, C obj) throws Exception {
-        log.trace( "bind {}", ifc.getName());
-
-        if( _client!=null ) {
-            // WE ARE ON THE CLIENT
-            UnicastRemoteObject.callHandler.exportObject(ifc,obj);
-            return;
-        }
-
-        // WE ARE ON THE CLIENT
-        if(_server==null) {
-
-            _server = new SocketServer();
-            _server.bind( port, UnicastRemoteObject.callHandler, null );
-
-        }
-
-        UnicastRemoteObject.callHandler.registerGlobal( ifc, obj);
+    protected SocketServer newServer() throws Exception {
+        return new SocketServer();
     }
 
     @Override
-    public <T> T lookup(Class<T> ifc) throws Exception  {
-        log.trace( "lookup {}", ifc.getName());
-
-        // WE ARE ON THE SERVER
-        if( _server!=null ) {
-
-            return UnicastRemoteObject.callHandler.getExportedObject(ifc)
-                    .orElseThrow( () ->
-                            new Exception( format("exported object with interface %s not found!", ifc.getName())));
-        }
-
-        // WE ARE ON THE CLIENT
-        if( _client==null ) {
-
-            final String host = getHost().orElseThrow( () -> new IllegalStateException("host has not been set!"));
-
-            _client = new SocketClient(host, port, UnicastRemoteObject.callHandler, null);
-
-        }
-
-        return _client.getGlobal(ifc);
+    protected SocketClient newClient(String host, int port, CallHandler callHandler, IProtocolFilter filter) throws Exception {
+        return new SocketClient(host, port, UnicastRemoteObject.callHandler, filter);
     }
-
-    @Override
-    public String[] list() throws Exception {
-
-        if( _server!=null || _client!=null ) {
-            return UnicastRemoteObject.callHandler.getExportedObjects()
-                    .stream()
-                    .map( ri -> ri.getClassName())
-                    .toArray( String[]::new );
-        }
-        throw new Exception("the registry is not connected yet!");
-    }
-
 
 }
